d44da70866760954929056e7208fe603d757135d
YazanFarrah <yazanfarrah@gmail.com>
2023-11-13 01:07:13 +0300

    World of dom intro.

---
+++ app.js
@@ -0,0 +1,35 @@
+// // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+// const container = document.querySelector('#container');
+// const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+// for (let i = 1; i <= 151; i++) {
+//     const pokemon = document.createElement('div');
+//     pokemon.classList.add('pokemon');
+//     const label = document.createElement('span');
+//     label.innerText = `#${i}`;
+//     const newImg = document.createElement('img');
+//     newImg.src = `${baseURL}${i}.png`
+
+//     pokemon.appendChild(newImg);
+//     pokemon.appendChild(label);
+//     container.appendChild(pokemon)
+// }
+
+const container = document.querySelector("#container");
+
+for (let i = 1; i <= 1017; i++) {
+  const pokemnon = document.createElement("div");
+  pokemnon.classList.add('pokemon');
+  const label = document.createElement("span");
+  label.innerText = `#${i}`;
+  const newImg = document.createElement("img");
+
+  newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+
+  pokemnon.appendChild(newImg);
+
+  pokemnon.appendChild(label);
+
+  container.appendChild(pokemnon);
+}
