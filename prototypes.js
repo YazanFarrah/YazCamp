5dcd73918f58b4649530c979b568eff45511c6f8
YazanFarrah <yazanfarrah@gmail.com>
2023-11-18 20:56:12 +0300

    File system

--- prototypes.js
+++
@@ -1,7 +0,0 @@
-String.prototype.yell = function () {
-  return `OMG ${this.toUpperCase()}!!! AKHHHH`;
-};
-
-Array.prototype.pop = function () {
-  return "Sorry I want that element. I will NEVER pop it off!";
-};
