0e29485c979ebc683dd1a232f2316b9d15e93384
YazanFarrah <yazanfarrah@gmail.com>
2023-11-19 16:34:13 +0300

    Ejs, loops, static assets in express

--- app.js
+++
@@ -1,3 +0,0 @@
-const math = require("./sadie");
-
-console.log(math.square(5));
--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
-exports.app = app;
+const redditData = require("./data.json");
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.get("/r/:subreddit", (req, res) => {
   const { subreddit } = req.params;
-  res.send(`<h2>You requested ${subreddit} subreddit</h2>`);
+  const data = redditData[subreddit];
+  if (!data) res.render("notFound", { subreddit });
+  res.render("subreddit", {
+    ...data,
+  });
 });
 
-app.get("/r/:subreddit/comments/:postId", (req, res) => {
-  const { subreddit, postId } = req.params;
-  res.send(
-    `<h2>You requested ${subreddit} subreddit in the postId: ${postId}</h2>`
-  );
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Sanjoob", "Babieee", "Kototoo", "Kittyyyy", "Pussy"];
+  res.render("cats", { cats });
 });
 
-app.post("/bitch", (req, res) => {
-  res.send("Akhh ur such a hoe!");
-});
+//to use templating engines "EJS here":
+//of course after installing it npm i ejs, no need to import it
+//express does this for us with app.set below:
+//default folder is views for what it looks for
+app.set("view engine", "ejs");
+//this below helps us running the app from anywhere without
+//having path problems in finding views in this case
+app.set("views", path.join(__dirname, "/views"));
+// __dirname takes the current path and joins with it
+//in this example "views"
 
-app.get("/search", (req, res) => {
-  const { q } = req.query;
-  if (!q) {
-    return res.send("Nothing found");
-  }
-  res.send(`<h1>Query result for ${q}</h1>`);
-});
-
-app.get("*", (req, res) => {
-  res.send("Bitch what do you want! ");
+app.get("/random", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("random", {
+    randNum: num,
+  });
 });
 
 app.listen(3000, () => {
